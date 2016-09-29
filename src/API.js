672cbe0c1e31023899590bc7591324ff2e2d478f
annzach <annreenazach@gmail.com>
2016-09-28 19:36:55 -0700

    new build

--- src/API.js
+++ src/API.js
@@ -3,7 +3,7 @@ import ServerActions from './actions/ServerActions';
 
 const API ={
  fetchWeather(){
-  $.get(`http://api.wunderground.com/api/40c0621a32103129/conditions/q/autoip.json`,(data,status)=>{
+  $.get(`https://api.wunderground.com/api/40c0621a32103129/conditions/q/autoip.json`,(data,status)=>{
     console.log('status',status);
     console.log('data',data);
     ServerActions.receiveWeather(data);
@@ -14,7 +14,7 @@ const API ={
    var str = place.split(',');
    var state = str[0];
    var city =str[1];
-   $.get(`http://api.wunderground.com/api/40c0621a32103129/conditions/q/${state}/${city}.json`,(data,status)=>{
+   $.get(`https://api.wunderground.com/api/40c0621a32103129/conditions/q/${state}/${city}.json`,(data,status)=>{
     console.log('status',status);
     console.log('data',data);
     ServerActions.receiveWeather(data);
